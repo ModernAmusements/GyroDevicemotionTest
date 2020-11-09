71f2fef3794891db199dfb298dd12641016fbd3a
ModernAmusements <st@zahnarzt-helden.de>
2020-11-09 16:28:51 +0100

    js fix

--- homePageScripts.js
+++ homePageScripts.js
@@ -1,126 +1,56 @@
 // Preloader
-$(function() {
-    $("#preload-homepage").click(function() {
-		$(this).fadeOut("300").remove();
-		if (
-			DeviceMotionEvent &&
-			typeof DeviceMotionEvent.requestPermission === "function"
-		) {
-			DeviceMotionEvent.requestPermission();
-		}
-
-		if (is_running) {
-			var x = e.clientX - $(this).offset().left + $(window).scrollLeft();
-			var y = e.clientY - $(this).offset().top + $(window).scrollTop();
-			var rY = map(x, 0, $(this).width(), -17, 17);
-			var rX = map(y, 0, $(this).height(), -17, 17);
-
-			$(this)
-				.children(".image")
-				.css(
-					"transform",
-					"rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)"
-				);
-			is_running = false;
-		} else {
-			var x = e.clientX - $(this).offset().left + $(window).scrollLeft();
-			var y = e.clientY - $(this).offset().top + $(window).scrollTop();
-			var rY = map(x, 0, $(this).width(), -17, 17);
-			var rX = map(y, 0, $(this).height(), -17, 17);
-
-			$(this)
-				.children(".image")
-				.css(
-					"transform",
-					"rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)"
-				);
-			is_running = true;
-		}
-		
-		function map(x, in_min, in_max, out_min, out_max) {
-			return ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-		}
-    });
-    if (!sessionStorage.getItem("homePagePreloader")) {
-        sessionStorage.setItem("homePagePreloader", true);
-        $("#preload-homepage").css("display", "block");
-        setTimeout(function() {
-            $("#preload-homepage").fadeOut();
-        }, 2000);
-        setTimeout(function() {
-            $("#preload-homepage").remove();
-        }, 2500);
-    } else {
-        $("#preload-homepage").css("display", "none");
-    }
+$(function () {
+  $("#preload-homepage").click(function () {
+    $(this).fadeOut("300").remove();
+  });
+  if (!sessionStorage.getItem("homePagePreloader")) {
+    sessionStorage.setItem("homePagePreloader", true);
+    $("#preload-homepage").css("display", "block");
+    setTimeout(function () {
+      $("#preload-homepage").fadeOut();
+    }, 2000);
+    setTimeout(function () {
+      $("#preload-homepage").remove();
+    }, 2500);
+  } else {
+    $("#preload-homepage").css("display", "none");
+  }
 });
 
-// $(function() {
-//     var card = $(".card");
+$(function () {
+  var card = $(".card");
 
-//     card.on("mousemove", function(e) {
-//         var x = e.clientX - $(this).offset().left + $(window).scrollLeft();
-//         var y = e.clientY - $(this).offset().top + $(window).scrollTop();
-//         var rY = map(x, 0, $(this).width(), -17, 17);
-//         var rX = map(y, 0, $(this).height(), -17, 17);
+  card.on("mousemove", function (e) {
+    var x = e.clientX - $(this).offset().left + $(window).scrollLeft();
+    var y = e.clientY - $(this).offset().top + $(window).scrollTop();
 
-//         $(this)
-//             .children(".image")
-//             .css(
-//                 "transform",
-//                 "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)"
-//             );
-//     });
+    var rY = map(x, 0, $(this).width(), -17, 17);
+    var rX = map(y, 0, $(this).height(), -17, 17);
 
-//     function map(x, in_min, in_max, out_min, out_max) {
-//         return ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-//     }
-// });
+    $(this)
+      .children(".image")
+      .css(
+        "transform",
+        "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)"
+      );
+  });
 
+  window.ondevicemotion = function(e) {
+    var x = e.clientX - $(this).offset().left + $(window).scrollLeft();
+    var y = e.clientY - $(this).offset().top + $(window).scrollTop();
 
-let is_running = false;
-var card = $(".card");
+    var rY = map(x, 0, $(this).width(), -17, 17);
+    var rX = map(y, 0, $(this).height(), -17, 17);
 
-card.on("mousemove", function(e) {
-            e.preventDefault();
-            // Request permission for iOS 13+ devices
-            if (
-                DeviceMotionEvent &&
-                typeof DeviceMotionEvent.requestPermission === "function"
-            ) {
-                DeviceMotionEvent.requestPermission();
-            }
+    $(this)
+      .children(".image")
+      .css(
+        "transform",
+        "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)"
+      );
+  };
 
-            if (is_running) {
-                var x = e.clientX - $(this).offset().left + $(window).scrollLeft();
-                var y = e.clientY - $(this).offset().top + $(window).scrollTop();
-                var rY = map(x, 0, $(this).width(), -17, 17);
-                var rX = map(y, 0, $(this).height(), -17, 17);
-
-                $(this)
-                    .children(".image")
-                    .css(
-                        "transform",
-                        "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)"
-                    );
-                is_running = false;
-            } else {
-				var x = e.clientX - $(this).offset().left + $(window).scrollLeft();
-                var y = e.clientY - $(this).offset().top + $(window).scrollTop();
-                var rY = map(x, 0, $(this).width(), -17, 17);
-                var rX = map(y, 0, $(this).height(), -17, 17);
-
-                $(this)
-                    .children(".image")
-                    .css(
-                        "transform",
-                        "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)"
-                    );
-                is_running = true;
-			}
-			
-			function map(x, in_min, in_max, out_min, out_max) {
-				return ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-			}
-		});
-		
+  function map(x, in_min, in_max, out_min, out_max) {
+    return ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  }
+});
