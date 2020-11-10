982b4307013f4e74b451bf62926705e29449ec5c
ModernAmusements <st@zahnarzt-helden.de>
2020-11-10 18:19:40 +0100

    small fix

--- src/js/homePageScripts.js
+++ src/js/homePageScripts.js
@@ -75,13 +75,13 @@ $(document).on('click touchstart', function(event) {
             xFixed = (Math.round(x * 10) / 10).toFixed();
             yFixed = (Math.round(y * 10) / 10).toFixed();
 
-            xGyro = 100 + xFixed * 4;
-            yGyro = 100 + yFixed * 4;
+            // xGyro = 100 + xFixed * 4;
+            // yGyro = 100 + yFixed * 4;
 
-            var rY = map(xGyro, 0, $('.card').width(), -17, 17);
-            var rX = map(yGyro, 0, $('.card').height(), -17, 17);
+            var rY = map(xFixed, 0, $('.card').width(), -17, 17);
+            var rX = map(yFixed, 0, $('.card').height(), -17, 17);
 
-            if (xGyro % 20 == 0) {
+            if (xFixed % 20 == 0) {
                 $('.card')
                 .children('.image')
                 .css(
