d11cb5d6d2b4984404b10ebad711044bf6c5bd43
ModernAmusements <st@zahnarzt-helden.de>
2020-11-11 14:51:52 +0100

    ok

--- app.js
+++ app.js
@@ -11101,7 +11101,7 @@ $(document).on('click touchstart', function (event) {
       xFixed = (Math.round(x * 10) / 10).toFixed();
       yFixed = (Math.round(y * 10) / 10).toFixed();
       xGyro = 500 + xFixed * 2;
-      yGyro = 500 + yFixed * 40 / 100;
+      yGyro = 500 + yFixed * 2;
       var rY = map(xGyro, 0, $('.card').width(), -17, 17);
       var rX = map(yGyro, 0, $('.card').height(), -17, 17);
       $('.card').children('.image').css('transform', 'rotateY(' + rY + 'deg)' + ' ' + 'rotateX(' + -rX + 'deg)');
