c6e9ebd337daf5ed0bad184bc9af977882337c55
ModernAmusements <st@zahnarzt-helden.de>
2020-11-11 12:03:20 +0100

    3

--- app.js
+++ app.js
@@ -11099,14 +11099,17 @@ $(document).on('click touchstart', function (event) {
     window.ondevicemotion = function (event) {
       // get mouse pos
       var x = event.accelerationIncludingGravity.x - $('.card').offset().left + $(window).scrollLeft();
-      var y = event.accelerationIncludingGravity.y - $('.card').offset().top + $(window).scrollTop(); // xFixed = (Math.round(x * 10) / 10).toFixed();
-      // yFixed = (Math.round(y * 10) / 10).toFixed();
-      // xGyro = 45 + xFixed * 2;
-      // yGyro = 60 + yFixed * 40 / 100;
+      var y = event.accelerationIncludingGravity.y - $('.card').offset().top + $(window).scrollTop();
+      xFixed = (Math.round(x * 10) / 10).toFixed();
+      yFixed = (Math.round(y * 10) / 10).toFixed();
+      xGyro = -100 + xFixed * 4;
+      yGyro = 100 + yFixed * 4;
+      var rY = map(xGyro, 0, $('.card').width(), -17, 17);
+      var rX = map(yGyro, 0, $('.card').height(), -17, 17);
 
-      var rY = map(x, 0, $('.card').width(), -17, 17);
-      var rX = map(y, 0, $('.card').height(), -17, 17);
-      $('.card').children('.image').css('transform', 'rotateY(' + rY + 'deg)' + ' ' + 'rotateX(' + -rX + 'deg)');
+      if (xGyro % 20 == 0) {
+        $('.card').children('.image').css('transform', 'rotateY(' + rY + 'deg)' + ' ' + 'rotateX(' + -rX + 'deg)');
+      }
     };
 
     is_running = true;
